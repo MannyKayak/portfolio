04749ca680ce73310ddd9bda4926b54b4c60d2e8
VirtualRedy <gustavo_alfonso@hotmail.it>
2025-04-23 12:43:33 +0200

    modified colors

--- app/context/constants.ts
+++ app/context/constants.ts
@@ -1,7 +1,10 @@
 export const PALETTE = {
-  color1: "#272946",
-  color2: "#aaea6c",
-  color3: "#e7ffee",
+  //   color1: "#272946",
+  //   color2: "#aaea6c",
+  //   color3: "#e7ffee",
+  color1: "#2a2f44",
+  color2: "#9bded3",
+  color3: "#effbff",
 };
 
 export const DIAGONAL_FACTOR = 1 / Math.sqrt(2);
